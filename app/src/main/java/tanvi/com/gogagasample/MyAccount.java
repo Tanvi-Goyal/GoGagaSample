2c3c8981d0767827586461ce2e1c20eec2ff0674
Tanvi-Goyal <goyaltanvi94@gmail.com>
2018-03-26 20:17:25 +0530

    NavHeader Icons Set

--- app/src/main/java/tanvi/com/gogagasample/MyAccount.java
+++ app/src/main/java/tanvi/com/gogagasample/MyAccount.java
@@ -2,10 +2,15 @@ package tanvi.com.gogagasample;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+
+import com.rom4ek.arcnavigationview.ArcNavigationView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import jp.wasabeef.blurry.Blurry;
@@ -18,6 +23,23 @@ public class MyAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+
+        ArcNavigationView navigationView =  findViewById(R.id.navigation_view);
+        navigationView.setItemIconTintList(null);
+
         image = findViewById(R.id.image);
 
         BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
