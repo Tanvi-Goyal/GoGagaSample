422e024e2500929261d8ed69febe451932357d39
Tanvi-Goyal <goyaltanvi94@gmail.com>
2018-03-26 19:20:18 +0530

    Blur ImageView Added

--- app/src/main/java/tanvi/com/gogagasample/MyAccount.java
+++ app/src/main/java/tanvi/com/gogagasample/MyAccount.java
@@ -1,15 +1,28 @@
 package tanvi.com.gogagasample;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import jp.wasabeef.blurry.Blurry;
 
 public class MyAccount extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
-       // ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle();
+        image = findViewById(R.id.image);
+
+        BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        Blurry.with(this).radius(40).from(bitmap).into(image);
+
     }
 }
