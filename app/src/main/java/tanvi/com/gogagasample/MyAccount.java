f240e49136f1eaca31d20285a6dd422d05bf4d50
Tanvi-Goyal <goyaltanvi94@gmail.com>
2018-03-26 18:54:17 +0530

    Initial Commit

---
+++ app/src/main/java/tanvi/com/gogagasample/MyAccount.java
@@ -0,0 +1,15 @@
+package tanvi.com.gogagasample;
+
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+       // ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle();
+    }
+}
